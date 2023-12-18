f50803e20c56c6704d4e06ead081eadd3b3cc4f1
Miriam Wahito Maina <miriammaina51@gmail.com>
2023-12-18 21:53:18 +0300

    added functionality for POSTing songs

---
+++ src/components/AddSong.js
@@ -0,0 +1,107 @@
+import React from "react";
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Modal from "react-bootstrap/Modal";
+
+import { BASE_URL } from "../utils";
+
+const AddSong = ({ showModal, handleClose, playlistId, playlistTitle }) => {
+  //initial data of the form which starts out as empty
+  const initialData = {
+    name: "",
+  };
+
+  // handling the addition of information to the form
+  const [formData, setFormData] = useState(initialData);
+
+  //isloading state
+  //const [isLoading, setIsLoading] = useState(false);
+
+  // handling the inputs
+  function handleChangeForm(event) {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+
+    //console.log(formData);
+  }
+
+  //handling the submitting
+  function handleForm(event) {
+    event.preventDefault();
+
+    //set loading to true before submitting
+    //setIsLoading(true);
+
+    const data = {
+      ...formData,
+      playlist_id: playlistId,
+      user_id: 1,
+    };
+
+    console.log(data);
+
+    fetch(`${BASE_URL}/songs`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+        handleClose();
+      })
+      .catch((err) => console.log(err));
+  }
+
+  return (
+    <>
+      <Modal show={showModal} onHide={handleClose}>
+        {/* Form handling eveything */}
+        <Form onSubmit={handleForm}>
+          <Modal.Header closeButton>
+            <Modal.Title>Add Song to {playlistTitle} Paylist</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            {/* form inside the modal body */}
+            <>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlInput1"
+              >
+                <Form.Label>Song Name and Artist </Form.Label>
+                <Form.Control
+                  required
+                  placeholder="Umbrella by Rihanna"
+                  autoFocus
+                  name="name"
+                  value={formData["name"]}
+                  onChange={handleChangeForm}
+                />
+              </Form.Group>
+            </>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button
+              variant="primary"
+              style={{ background: "#526a40", border: "0px" }}
+              type="submit"
+              //   isLoading = {isLoading}
+            >
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Form>
+      </Modal>
+    </>
+  );
+};
+
+export default AddSong;
--- src/components/PlaylistCard.js
+++ src/components/PlaylistCard.js
@@ -1,35 +1,65 @@
 import React from "react";
 import { useState } from "react";
 import { Button, Card } from "react-bootstrap";
+import AddSong from "./AddSong";
 
-const PlaylistCard = ({ image, title, description, rating, date_created }) => {
+const PlaylistCard = ({
+  id,
+  image,
+  title,
+  description,
+  rating,
+  date_created,
+}) => {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleShowModal = () => setShowModal(true);
+  const handleCloseModal = () => setShowModal(false);
+
   return (
-    <Card
-      style={{
-        width: "18rem",
-        padding: "10px",
-        margin: "10px",
-        background: "#11150d",
-        color: "white",
-      }}
-    >
-      <Card.Img variant="top" src={image} />
-      <Card.Body>
-        <Card.Title> {title}</Card.Title>
-        <Card.Text>Description : {description}</Card.Text>
+    <>
+      <Card
+        style={{
+          width: "18rem",
+          padding: "10px",
+          margin: "10px",
+          background: "#11150d",
+          color: "white",
+        }}
+      >
+        <Card.Img variant="top" src={image} />
+        <Card.Body>
+          <Card.Title> {title}</Card.Title>
+          <Card.Text>Description : {description}</Card.Text>
 
-        <br />
+          <br />
 
-        <Card.Text>Rating : {rating}</Card.Text>
-        <Card.Text>Date Created : {date_created}</Card.Text>
-        <Button
-          variant="primary"
-          style={{ background: "#526a40", border: "0px" }}
-        >
-          View playlist
-        </Button>
-      </Card.Body>
-    </Card>
+          <Card.Text>Rating : {rating}</Card.Text>
+          <Card.Text>Date Created : {date_created}</Card.Text>
+          <Button
+            variant="primary"
+            style={{ background: "#526a40", border: "0px" }}
+          >
+            View playlist
+          </Button>
+
+          <Button
+            variant="primary"
+            style={{ background: "#526a40", border: "0px", marginLeft: "10px" }}
+            onClick={handleShowModal}
+          >
+            Add Song
+          </Button>
+
+          <AddSong
+            showModal={showModal}
+            handleClose={handleCloseModal}
+            playlistId={id}
+            playlistTitle={title}
+          />
+        </Card.Body>
+      </Card>
+    </>
   );
 };
 
