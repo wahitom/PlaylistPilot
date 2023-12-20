eff1ca3c9386fe4f3032d93640147a82b8bccf27
Miriam Wahito Maina <miriammaina51@gmail.com>
2023-12-20 18:12:16 +0300

    edited sidebar

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Home from "./pages/Home";
 import AddPlaylist from "./pages/AddPlaylist";
 import About from "./pages/About";
 import Navbar from "./components/Navbar";
-import Sidebar from "./components/Sidebar";
+//import Sidebar from "./components/Sidebar";
 import { BASE_URL } from "./utils";
 import PlaylistSongs from "./components/PlayistSongs";
 
@@ -34,7 +34,7 @@ function App() {
       <Navbar toggleSidebar={toggleSidebar} />
 
       <div style={{ display: "flex", height: "100vh" }}>
-        {isSidebarVisible && <Sidebar playlists={playlists} />}
+        {/*  {isSidebarVisible && <Sidebar playlists={playlists} />} */}
 
         <div style={{ flex: 2, overflow: "auto" }}>
           <div
